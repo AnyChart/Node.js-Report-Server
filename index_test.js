78c1201768e11e990786a139a83e81006f8e07aa
blackart <blackart.ru@gmail.com>
2017-09-18 17:11:10 +0800

    tests

--- index_test.js
+++ index_test.js
@@ -1,12 +1,79 @@
 #!/usr/bin/env node
 var jsdom = require('jsdom').jsdom;
+var opentype = require('opentype.js');
+// var defaultFontsDir = __dirname + '/node_modules/anychart-nodejs/fonts';
 
 var rootDoc = jsdom('<body><div id="container"></div></body>');
 var window = rootDoc.defaultView;
 var anychart = require('../ACDVF/out/anychart-base.min.js')(window);
 var anychart_nodejs = require('../AnyChart-NodeJS')(anychart);
 
+// var fs = require('fs');
+// var fonts = {};
+// function anychartify(doc) {
+//   doc.createElementNS = function(ns, tagName) {
+//     var elem = doc.createElement(tagName);
+//     elem.getBBox = function() {
+//       var text = elem.textContent;
+//       var fontSize = parseFloat(elem.getAttribute('font-size'));
+//       var fontFamily = elem.getAttribute('font-family');
+//       if (fontFamily) fontFamily = fontFamily.toLowerCase();
+//       var fontWeight = elem.getAttribute('font-weight');
+//       if (fontWeight) fontWeight = fontWeight.toLowerCase();
+//       var fontStyle = elem.getAttribute('font-style');
+//       if (fontStyle) fontStyle = fontStyle.toLowerCase();
+//
+//       var fontsArr = fontFamily.split(', ');
+//
+//       var font;
+//       for (var i = 0, len = fontsArr.length; i < len; i++) {
+//         var name = fontsArr[i] + (fontWeight == 'normal' || !isNaN(+fontWeight) ? '' : ' ' + fontWeight) + (fontStyle == 'normal' ? '' : ' ' + fontStyle);
+//         if (font = fonts[name])
+//           break;
+//       }
+//
+//       if (!font)
+//         font = fonts['verdana'];
+//
+//       var scale = 1 / font.unitsPerEm * fontSize;
+//
+//       var top = -font.ascender * scale;
+//       var height = Math.abs(top) + Math.abs(font.descender * scale);
+//
+//       var width = 0;
+//
+//       font.forEachGlyph(text, 0, 0, fontSize, undefined, function(glyph, x, y, fontSize, options) {
+//         var metrics = glyph.getMetrics();
+//         metrics.xMin *= scale;
+//         metrics.xMax *= scale;
+//         metrics.leftSideBearing *= scale;
+//         metrics.rightSideBearing *= scale;
+//
+//         width += Math.abs(metrics.xMax - metrics.xMin) + metrics.leftSideBearing + metrics.rightSideBearing
+//       });
+//
+//       return {x: 0, y: top, width: width, height: height};
+//     };
+//     return elem;
+//   };
+// }
+// function loadDefaultFontsSync() {
+//   var fontFilesList = fs.readdirSync(defaultFontsDir);
+//
+//   for (var i = 0, len = fontFilesList.length; i < len; i++) {
+//     var fileName = fontFilesList[i];
+//     var font = opentype.loadSync(defaultFontsDir + '/' + fileName);
+//     fonts[font.names.fullName.en.toLowerCase()] = font;
+//   }
+//
+//   return fonts;
+// }
+
 var data = '{"chart":{"enabled":true,"credits":{"text":"AnyChart","url":"https://www.anychart.com/?utm_source=registered","alt":"AnyChart - JavaScript Charts designed to be embedded and integrated","imgAlt":"AnyChart - JavaScript Charts","logoSrc":"https://static.anychart.com/logo.png","enabled":true},"xScale":0,"yScale":1,"series":[{"enabled":true,"seriesType":"line","data":[1,2,3],"xScale":0,"yScale":1}],"type":"cartesian"}}';
+
+// loadDefaultFontsSync();
+// anychartify(rootDoc);
+// anychart.global(window);
 
 var params = {
   type: 'png',
@@ -15,18 +82,36 @@ var params = {
   document: rootDoc
 };
 
-var chartCount = 1000;
+var chartCount = 5000;
 var cur = 0;
 
-// var inertval = setInterval(function() {
+
+var inertval = setInterval(function() {
+  // var target = anychart.fromJson(data);
+  // target.container('container').draw();
+  // target.dispose();
+  //
+  // if (cur > chartCount) {
+  //   clearInterval(inertval);
+  //   console.log('interval cleared');
+  //   console.log(rootDoc.getElementById('container').innerHTML);
+  // } else {
+  //   cur++;
+  //   console.log(cur);
+  // }
+
   anychart_nodejs.exportTo(data, params, function(err, data) {
     if (cur > chartCount) {
-      // clearInterval(inertval);
+      clearInterval(inertval);
       console.log('interval cleared');
     } else {
       cur++;
       console.log(cur, err, data);
     }
   });
-// }, 1);
+}, 1);
+
+// setInterval(function() {
+  // console.log();
+// }, 5000);
                                                          
